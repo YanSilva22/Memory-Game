5a625be4c3ffe056bbb8763b450a4fdfcc980e0b
YanSilva22 <yanviniciusjeba9@gmail.com>
2025-07-21 10:50:01 -0300

    Adiciona funções a pagina inicial
    Cria função de verificação de formario
    Cria função de mudar de pagina
    Adiciona caminho do arquivo js no index

--- js/initial.js
+++ js/initial.js
@@ -1,1 +1,20 @@
+const input = document.querySelector('.input-login');
+const button = document.querySelector('.button-submit');
+const form = document.querySelector('.login-form');
 
+const inputValidation = ({ target }) => {
+  if (target.value.length > 2) {
+    button.removeAttribute('disabled');
+    return;
+  }
+  button.setAttribute('disabled', '');
+};
+
+const formSubmit = (event) => {
+  event.preventDefault();
+  localStorage.setItem('username', input.value);
+  window.location = 'pages/game.html';
+};
+
+input.addEventListener('input', inputValidation);
+form.addEventListener('submit', formSubmit);
